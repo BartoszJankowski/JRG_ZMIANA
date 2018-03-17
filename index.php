bed9995761de7b05fa18684cab7e4b5beca25678
BartoszJankowski <jankowski.ba@gmail.com>
2018-03-17 20:53:51 +0100

    -

--- index.php
+++ index.php
@@ -93,8 +93,8 @@ if(isset($_POST['addJrg'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-              <link rel="stylesheet" type="text/css" href="css/main.css" />
-                <link rel="stylesheet" type="text/css" href="css/login.css" />
+              <link rel="stylesheet" type="text/css" href="css/main.css?ver=<?php echo time() ?>" />
+                <link rel="stylesheet" type="text/css" href="css/login.css?ver=<?php echo time() ?>" />
                     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" /> 
                         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&amp;subset=latin-ext" rel="stylesheet">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
